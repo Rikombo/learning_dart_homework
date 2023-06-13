6ffec2b2b6bfb7c2da73365dace54e0ffc8e45e3
Valentins Stepanovs <valentin.stepano@gmail.com>
2023-06-13 21:26:38 +0300

    Edited prime number checking function

--- lib/Homework/TasksWithLists/task9.dart
+++ lib/Homework/TasksWithLists/task9.dart
@@ -1,8 +1,14 @@
 // Write a function that accepts a number and returns true if it is a prime number, false otherwise.
 
 void main() {
-  int number = 35;
+  int number = 2;
   print(primeCheck(number));
 }
 
-bool primeCheck(int number) => number % 2 == 0;
+bool primeCheck(int number) {
+  if (number < 2) return false;
+  for (int i = 2; i * i <= number; i++) {
+    if (number % i == 0) return false;
+  }
+  return true;
+}
